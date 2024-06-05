a8b8bc84b029fe9723a434aa04fc455f5b7777b2
[afaureve] <[tom.favereautpe@gmail.com]>
2024-06-05 15:42:07 +0200

    huffman

--- Huffman.cpp
+++ Huffman.cpp
@@ -3,6 +3,51 @@
 //
 
 #include "Huffman.h"
+#include <vector>
+#include "ByteReading.h"
 
-namespace huffman {
-} // huffman
+
+Huffman::Huffman(const std::vector<char>& sector){
+    bool bit0 = sector[4] & 0b00000001;  // Vérifie le bit 0
+    bool bit1 = sector[4] & 0b00000010;  // Vérifie le bit 1
+    bool bit2 = sector[4] & 0b00000100;  // Vérifie le bit 2
+    bool bit3 = sector[4] & 0b00001000;  // Vérifie le bit 3
+    bool bit4 = sector[4] & 0b00010000;  // Vérifie le bit 3
+
+    if (bit0){
+        indice_huffman = 0;
+    } else if (bit1){
+        indice_huffman = 1;
+    } else if (bit2){
+        indice_huffman = 2;
+    } else if (bit3){
+        indice_huffman = 3;
+    }
+
+    type = bit4;
+
+    for (int i = 0; i < 16; i++){
+        nb_symboles[i] = ByteReading::readBytes(sector, 0x05+i, 1);
+    }
+
+    buildCode(sector);
+
+
+}
+
+void Huffman::buildCode(const std::vector<char>& sector) {
+    int count = 0;
+    uint16_t code = 0;
+    for (int nb : nb_symboles){
+        for (int j = 0; j < nb; j++){
+            huffman_codes[(uint8_t) sector[21+count]] = code;
+            code++;
+            count++;
+        }
+        code <<= 1;
+    }
+}
+
+std::vector<uint8_t> Huffman::readBlock(const uint8_t& previousDC, const std::vector<char> &sector) {
+    
+}
--- Huffman.h
+++ Huffman.h
@@ -5,8 +5,9 @@
 #ifndef FILE_COMPRESSION_CPP_HUFFMAN_H
 #define FILE_COMPRESSION_CPP_HUFFMAN_H
 #include <cstdint>
+#include <vector>
+#include <unordered_map>
 
-namespace huffman {
 
 
     typedef struct _huff_table_t {
@@ -20,17 +21,25 @@ namespace huffman {
 
 
     class Huffman {
-
+    private:
         //sur un seul octet on a ces information
-        uint8_t indice_huffman;
-        bool type; // 0 = DC, 1 = AC
+        uint8_t indice_huffman{};
+        bool type{}; // 0 = DC, 1 = AC
 
-        uint8_t nb_symboles[16];
+        uint8_t nb_symboles[16]{};
+
+        std::unordered_map<uint8_t , uint16_t> huffman_codes;
+
+        void buildCode(const std::vector<char>& sector);
 
 
+
+    public:
+        Huffman(const std::vector<char>& sector);
+
+        std::vector<uint8_t> readBlock(const uint8_t& previousDC, const std::vector<char>& sector);
 
     };
 
-} // huffman
 
 #endif //FILE_COMPRESSION_CPP_HUFFMAN_H
